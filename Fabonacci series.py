ef7a1568c4e7016b845003910a47a6f1f0e871ad
Pushkaraj Dhoot <89027620+Push777@users.noreply.github.com>
2021-09-30 19:30:10 +0530

    Update Fabonacci series.py

--- Fabonacci series.py
+++ Fabonacci series.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
 
 num=int(input())
 first=0
@@ -14,16 +9,4 @@ for i in range(num):
     second=x+second
     
     
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
