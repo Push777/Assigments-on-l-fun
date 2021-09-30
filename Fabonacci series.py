80bf807b55df98abc76f4ac7e4c5dc628085b8cf
Pushkaraj Dhoot <89027620+Push777@users.noreply.github.com>
2021-09-30 19:29:27 +0530

    Add files via upload

---
+++ Fabonacci series.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+num=int(input())
+first=0
+second=1
+for i in range(num):
+    print(first)
+    x = first
+    first=second
+    second=x+second
+    
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
