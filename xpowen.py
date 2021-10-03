24a7eac72e7d6844d05999eccfe211eb707ffa13
Pushkaraj Dhoot <89027620+Push777@users.noreply.github.com>
2021-10-03 17:56:52 +0530

    Add files via upload

---
+++ xpowen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+class power:
+    def __init__(self,x,n):
+        self.x=x
+        self.n=n
+    x=int(input('enter the value of x: '))
+    n=int(input('enter the value of n: ') )
+    print(x**n)
+
+
+# In[ ]:
+
+
+
+
